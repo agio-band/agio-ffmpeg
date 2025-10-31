25b91422addad2a93da892028d44cee1dc7f12f7
paulwinex <paulwinex@gmail.com>
2025-10-31 16:52:56 +0300

    fix callbacks

--- agio_ffmpeg/callbacks/package_callbacks.py
+++ agio_ffmpeg/callbacks/package_callbacks.py
@@ -1,14 +1,19 @@
-from agio.tools import app_dirs
+from agio.core.workspaces import AWorkspaceManager, APackageManager
 from agio_ffmpeg.lib import binary_helper
 
 
-def on_installed(package):
+def on_installed(package: APackageManager, ws_manager: AWorkspaceManager):
+    # TODO use cache
+    if not ws_manager.bin_path.exists():
+        raise FileExistsError('Binary dir not exists. Workspace not installed?')
     # download ffmpeg binary
-    binary_helper.download_binary(app_dirs.binary_files_dir())
+    print('Download ffmpeg to', ws_manager.bin_path)
+    binary_helper.download_binary(ws_manager.bin_path.as_posix())
 
 
-def after_uninstalling(package):
+def after_uninstalling(package: APackageManager, ws_manager: AWorkspaceManager):
     # delete ffmpeg binary
-    binary_helper.cleanup_binary(app_dirs.binary_files_dir())
+    bin_path = ws_manager.bin_path.as_posix()
+    binary_helper.cleanup_binary(bin_path)
 
 
