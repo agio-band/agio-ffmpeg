528f785bba4c834fd67208e1b93d450954fa8bea
paulwinex <paulwinex@gmail.com>
2025-11-22 12:11:26 +0300

    fix ffmpeg tools

--- agio_ffmpeg/tools.py
+++ agio_ffmpeg/tools.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 import os
 from agio.core.workspaces import AWorkspaceManager
+from agio.tools import paths
 
 SUFFIX = '.exe' if os.name == 'nt' else ''
 
@@ -9,8 +10,11 @@ def get_ffmpeg_tool(name="ffmpeg"):
     binary = get_ffmpeg_binary_dir().joinpath(name).with_suffix(SUFFIX)
     if not binary.exists():
         raise FileNotFoundError(f'ffmpeg tool not found "{name}"')
+    return paths.expand_windows_path(binary)
 
 
 def get_ffmpeg_binary_dir() -> Path:
     manager = AWorkspaceManager.current()
+    if not manager:
+        raise RuntimeError('No current workspace defined')
     return manager.bin_path
